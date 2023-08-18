24a4c99e79cac7413f0a0b765a4d6d7dd82f3bc2
Ivan Tammaro <tammaroivan@gmail.com>
2023-08-18 10:43:07 +1000

    Implement basic Terminal UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import ContractInput from "@/components/ContractInput";
 import Collections from "@/components/Pagebar/Collections";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import Image from "next/image";
+import Terminal from "@/components/ui/Terminal";
 
 const tabs: Record<string, string> = {
   functions: "Functions",
@@ -17,40 +18,43 @@ export default function Home() {
   return (
     <main className="flex flex-1">
       <Collections />
-      <div className="flex flex-col p-3 w-full gap-7">
-        <Breadcrumb
-          contractName="Counter contract"
-          folderName="Basic use case"
-          contractInvocationName="Get current counter"
-        />
-        <ContractInput />
-        <Tabs defaultValue="functions" className="">
-          <TabsList className="bg-white text-black dark:bg-background dark:text-white">
-            {Object.keys(tabs).map((tab) => (
-              <TabsTrigger
-                key={tab}
-                value={tab}
-                className="data-[state=active]:bg-background data-[state=active]:dark:bg-white data-[state=active]:text-white data-[state=active]:dark:text-black  rounded-md"
-              >
-                {tabs[tab]}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          <TabsContent value="functions">
-            <div className="flex justify-center mt-36 flex-1 gap-8">
-              <Image
-                src="/moon.svg"
-                alt="Load contract image"
-                width={300}
-                height={300}
-              />
-              <div className="flex flex-col justify-center text-primary font-black text-6xl">
-                <h2>Let&apos;s Load</h2>
-                <h2>Your Contract</h2>
+      <div className="flex flex-col justify-between w-full gap-7">
+        <div className="flex flex-col p-3 w-full gap-7">
+          <Breadcrumb
+            contractName="Counter contract"
+            folderName="Basic use case"
+            contractInvocationName="Get current counter"
+          />
+          <ContractInput />
+          <Tabs defaultValue="functions" className="">
+            <TabsList className="bg-white text-black dark:bg-background dark:text-white">
+              {Object.keys(tabs).map((tab) => (
+                <TabsTrigger
+                  key={tab}
+                  value={tab}
+                  className="data-[state=active]:bg-background data-[state=active]:dark:bg-white data-[state=active]:text-white data-[state=active]:dark:text-black  rounded-md"
+                >
+                  {tabs[tab]}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            <TabsContent value="functions">
+              <div className="flex justify-center mt-36 flex-1 gap-8">
+                <Image
+                  src="/moon.svg"
+                  alt="Load contract image"
+                  width={300}
+                  height={300}
+                />
+                <div className="flex flex-col justify-center text-primary font-black text-6xl">
+                  <h2>Let&apos;s Load</h2>
+                  <h2>Your Contract</h2>
+                </div>
               </div>
-            </div>
-          </TabsContent>
-        </Tabs>
+            </TabsContent>
+          </Tabs>
+        </div>
+        <Terminal />
       </div>
     </main>
   );
---
+++ src/components/ui/Terminal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Terminal = () => {
+  return (
+    <div className="h-[300px] border-t p-4 text-background-300 dark:border-t-background-100">
+      Welcome to keizai 0.1.0 - OUTPUT
+    </div>
+  );
+};
+
+export default Terminal;
